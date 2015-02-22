ff9959e4425f9f7e8b94bc7e1cf89a5319dbf94c
geogaffer <jharrop@acm.org>
2015-02-21 23:58:41 -0800

    code additions

--- project plots.R
+++ project plots.R
@@ -15,3 +15,20 @@ q1_plot <- function(table1) {
          xlab="Year", ylab="PM2.5  Emission (million tons)",
          xlim=c(1998,2008), ylim=c(3,8))
 }
+
+#Baltimore City, Maryland (fips == "24510"
+
+q2_plot <- function(table2) {
+    plot(table2[,1], table2[,2]/1000, type="b", col="blue", 
+         main="Changes in Baltimore PM2.5 Emissions",
+         xlab="Year", ylab="PM2.5  Emission (thousand tons)",
+         xlim=c(1998,2008), ylim=c(1.6, 3.6))
+}
+
+q3_plot <- function(table3) {
+    p <- ggplot(table3, aes(x=Group.1, y=x, colour=type))
+    p <- p + geom_line()
+    p <- p + labs(x="Year", y="PM2.5 Emissions (tons)")
+    p <- p + ggtitle("Categories of Baltimore PM2.5 Emissions")
+    p
+}
